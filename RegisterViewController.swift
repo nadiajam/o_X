f78b7b56ab2edc5879c98aae1a02e4d8fa593c6f
nadiajam <nadiajamrozik2018@u.northwestern.edu>
2016-07-05 14:03:36 +0200

    added Alamofire

--- RegisterViewController.swift
+++ RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class RegisterViewController: UIViewController {
 
@@ -29,6 +30,19 @@ class RegisterViewController: UIViewController {
                 window?.rootViewController = viewController
             }
         })
+        
+        Alamofire.request(.GET, "https://httpbin.org/get")          //this is a get method
+        Alamofire.request(.GET, "https://httpbin.org/get", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
     }
 
     override func viewDidLoad() {
